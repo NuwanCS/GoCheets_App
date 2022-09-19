07c1ac749cb2aaee4d0dc1aa431cd06c910d1daf
Nuwan Senevirathne <smpg.nuwan@gmail.com>
2022-09-20 04:54:53 +0530

    contact form

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Form, FormGroup, Input, ListGroup, ListGroupItem } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+
+import "../styles/contact.css";
+
+const socialLinks = [
+  {
+    url: "#",
+    icon: "ri-facebook-line",
+  },
+  {
+    url: "#",
+    icon: "ri-instagram-line",
+  },
+  {
+    url: "#",
+    icon: "ri-linkedin-line",
+  },
+  {
+    url: "#",
+    icon: "ri-twitter-line",
+  },
+];
+
+const branches = [
+  {
+    Name: "Kandy",
+    phone: "94222345678",
+  },
+
+  {
+    Name: "Galle",
+    phone: "94222345678",
+  },
+
+  {
+    Name: "Gampaha",
+    phone: "94222345678",
+  },
+  {
+    Name: "Kurunegala",
+    phone: "94222345678",
+  },
+  {
+    Name: "Jaffna",
+    phone: "94222345678",
+  },
+];
+
+
+
+const Contact = () => {
+  return (
+    <Helmet title="Contact">
+      <CommonSection title="Contact" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="7" md="7">
+              <h6 className="fw-bold mb-4">Get In Touch</h6>
+
+              <Form>
+                <FormGroup className="contact__form">
+                  <Input placeholder="Your Name" type="text" />
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <Input placeholder="Email" type="email" />
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <textarea
+                    rows="5"
+                    placeholder="Message"
+                    className="textarea"
+                  ></textarea>
+                </FormGroup>
+
+                <button className=" contact__btn" type="submit">
+                  Send Message
+                </button>
+              </Form>
+            </Col>
+
+            <Col lg="5" md="5">
+              <div className="contact__info">
+                <h6 className="fw-bold">Contact Information</h6>
+                <p className="section__description mb-0">
+                  123 Nawala Rd, Nugegoda
+                </p>
+                <div className=" d-flex align-items-center gap-2">
+                  <h6 className="fs-6 mb-0">Phone:</h6>
+                  <p className="section__description mb-0">+94112345678</p>
+                </div>
+
+                <div className=" d-flex align-items-center gap-2">
+                  <h6 className="mb-0 fs-6">Email:</h6>
+                  <p className="section__description mb-0">contact@gocheeta.com</p>
+                </div>
+
+                <h6 className="fw-bold mt-4">Our Branches</h6>
+
+                <div className="mb-4">
+              <ListGroup>
+                {branches.map((item, index) => (
+                  <ListGroupItem key={index} className="p-0 mt-3 quick__link">
+                    <span>{item.Name} : {item.phone}</span>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </div>
+            <h6 className="fw-bold mt-4">Follow Us</h6>
+                <div className=" d-flex align-items-center gap-4 mt-3">
+                  {socialLinks.map((item, index) => (
+                    <Link
+                      to={item.url}
+                      key={index}
+                      className="social__link-icon"
+                    >
+                      <i className={item.icon}></i>
+                    </Link>
+                  ))}
+                </div>
+
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Contact;
