ded8277f2f4b72946b84ddb648dcc2a544f6cd4c
Nuwan Senevirathne <smpg.nuwan@gmail.com>
2022-09-20 20:45:47 +0530

    Minor content changes in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,8 +57,8 @@ const Home = () => {
         <Container>
           <Row>
             <Col lg="12" className="text-center mb-5">
-              <h6 className="section__subtitle">Come with</h6>
-              <h2 className="section__title">Hot Offers</h2>
+              <h6 className="section__subtitle">Checkout our</h6>
+              <h2 className="section__title">Car Fleet</h2>
             </Col>
 
             {carData.slice(0, 6).map((item) => (
